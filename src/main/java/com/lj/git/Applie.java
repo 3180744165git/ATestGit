d6a041a68313639003ea55b2f682a179a15a3d1d
eclipse_user <eclipse_user@lj.com>
2020-05-27 18:00:51 +0800

    one commit

---
+++ src/main/java/com/lj/git/Applie.java
@@ -0,0 +1,14 @@
+package com.lj.git;
+
+public class Applie {
+
+	private String color = "red";
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
